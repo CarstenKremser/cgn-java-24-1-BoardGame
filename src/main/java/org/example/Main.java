4abe189abc03fd55c488715e806491daf451766c
Carsten Kremser <github@carstenkremser.de>
2024-03-11 12:14:58 +0100

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public class Main {
+
+
+    final static int XSIZE = 3;
+    final static int YSIZE = 2;
+
+    boolean[][] board = new boolean[XSIZE][YSIZE];
+
+
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
